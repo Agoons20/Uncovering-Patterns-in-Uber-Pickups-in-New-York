a855a695d6db2cd203df606ae96952106b4483a1
Duchoma Agoons <56331600+Agoons20@users.noreply.github.com>
2025-07-16 16:25:11 -0400

    data_preprocessing.py

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -18,11 +18,11 @@ df = df.rename(columns={
     'sd': 'snow_depth'
 })
 
-# Update holiday values for readability
+# Update holiday values for readability. Replace 'Y' with 'Yes' and 'N' with 'No'
 df['holiday'] = df['holiday'].replace('Y', 'Yes')
 df['holiday'] = df['holiday'].replace('N', 'No')
 
-# Handle missing values in borough (correcting the original error)
+# Handle missing values in 'borough' variable by replacing them with 'Unknown'. Deletion means deleting ~10% of data
 df['borough'] = df['borough'].fillna('Unknown')  # Replace NaN with 'Unknown'
 
 # Display borough value counts
